2d1fa0509645abf895504deefab9f7bbd7b7eaca
tapi0ka <t-kanagawa@stylagy.co.jp>
2020-07-22 00:23:20 +0900

    はじめてのこみっと！

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,22 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+// dotファイルから環境変数を取得
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
+      },
+    },
+  ],
 }
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = ({location, children}) => {
+  return (
+    <div>
+      aaa
+    </div>
+  )
+}
+
+export default Layout;
